f66c77bf732cd685cc6ba240347ea7637911f17e
unknown <afloridav@gmail.com>
2021-04-30 15:22:45 -0500

    text parser started. can display text in the textbox.

--- public/STILTON/core.js
+++ public/STILTON/core.js
@@ -2,20 +2,45 @@ console.log(`loading STILTON...`)
 //init STILTON vars.
 const w = `1000px`
 const h = `700px`
-var script = ``;
+//the core script.
+var script = [];
 var characterList = [];
-
 class Character {
     constructor() 
     {
-
     }
 }
+
+///////////////////////////////////////////////////////////CORE STILTON VARS////////////////////////////////////////////////
+//sayMode controls how text is printed out into the window.
+var sayMode = false;
+//what line is STILTON about to read.
+var line = 0
+//what character is STILTON currently focused on.
+var targetCharacter;
+//rollout text speed, in milliseconds.
+var rollout = 100;
+
+//sound controllers
+
 
 //load the script into memory.
 fetch(`./STILTON/script.txt`)
     .then(async (r) => {
-        script = await r.text()
+        var text = await r.text()
+        script = text.split(`\r\n`)
+        var done = false;
+        var i = 0;
+        while(!done)
+        {
+            if(script[i][0]===`#` || !script[i][0]) 
+            {
+                script.splice(i,1)
+            }
+                else i++
+
+            if(i==script.length) done = true
+        }
     })
 //load the characters into memory.
 fetch(`./STILTON/characters.txt`)
@@ -45,30 +70,30 @@ fetch(`./STILTON/characters.txt`)
             
             if(rows[i].match(/^\:{2}/))
             {
-                console.log(`>>>>>>>>>>>>exting initMode.`)
+                //console.log(`>>>>>>>>>>>>exting initMode.`)
                 initMode = false
                 characterList.push(chara)
             }
             //once initMode is entered, parse the following lines as properties.
             if(initMode)
             {
-                console.log(`parsing as initMode...`)
+                //console.log(`parsing as initMode...`)
                 var prop = rows[i].match(/(\w+)\:/)[1]
                 var val = rows[i].match(/\w+\:\s+(.*)$/)[1]
-                console.log(`prop ${prop} val ${val}`)
+                //console.log(`prop ${prop} val ${val}`)
                 chara[`${prop}`] = val
-                console.log(chara)
+                //console.log(chara)
             }
 
 
             //search for a line with a string of characters, and then a colon after. This starts initMode, which allows STILTON to parse following lines as properties.
             if(rows[i].match(/(\w+)\:{2}/) && !initMode) 
             {
-                console.log(`starting initMode...`)
+                //console.log(`starting initMode...`)
                 initMode = true
                 var chara = new Character()
                 chara.id = rows[i].match(/(\w+)\:{2}/)[1]
-                console.log(chara)
+                //console.log(chara)
             }
 
 
@@ -79,14 +104,46 @@ fetch(`./STILTON/characters.txt`)
 
 
     
-console.log(`initing charas...`)
-var det = new Character(`Antonia`,`det`,`#005500`)
 
 
 //perform the next action.
 //this is the core parser head for STILTON, and it's called everytime the container div is clicked.
 let step = async () => {
-    console.log(`steppin.`)
+    //start sayMode if the command is called.
+    if(script[line].match(/\w+\:/))
+    {
+        sayMode = true;
+        targetCharacter = script[line].match(/(\w+)\:/)[1]
+        console.log(`sayMode called for ${targetCharacter}`)
+        line++
+    }
+
+    if(sayMode)
+    {
+        console.log(`${targetCharacter}: ${script[line]}`)
+        var tb = document.getElementById(`tb`)
+        var currStr = ``
+        //rollout text.
+        if(rollout!==0)
+        {
+            for(var i = 0; i < script[line].length;i++)
+            {
+                currStr +=script[line][i]
+                await new Promise((res,rej) => {
+                    setTimeout(() => {
+                        res()
+                    },rollout)
+                })
+                tb.innerHTML=currStr
+
+            }
+        } else 
+        {
+            tb.innerHTML=script[line]
+        }
+    }
+
+    line++;
 
     //moveI()
 }
@@ -102,9 +159,21 @@ let init = () => {
     s.style.borderColor = `aquamarine`
     s.style.width = w
     s.style.height = h
-    s.innerHTML = `sfsdf`
     s.style.position = `relative`
 
+    //add the textbox child.
+    var tb = document.createElement(`div`)
+    tb.id = `tb`
+    s.appendChild(tb)
+    tb.style.borderStyle = `solid`
+    tb.style.borderColor = `#FF0000`
+    tb.style.backgroundColor = `#555555`
+    tb.style.width = `973px`
+    tb.style.height = `200px`
+    tb.style.top = `69%`
+    tb.style.left = `1%`
+    //tb.style.opacity = `30%`
+    tb.style.position = `absolute`
     console.log(`...init done.`)
     //init characters.
 
@@ -122,7 +191,7 @@ let drawI = (c) => {
     var s = document.getElementById("STILTON")
     var i = document.createElement(`img`)
     console.log(`drawing engie`)
-    i.src = `engie.gif`
+    i.src = `./STILTON/img/engie.gif`
     i.style.width = `100px`
     i.id = `eng`
     i.style.position = `absolute`
